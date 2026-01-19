a788fe22f27ac33c13612129b2f2a5474a82603a
Dariusz Malinowski <dariusz.m.malinowski@gmail.com>
2026-01-19 17:18:18 +0100

    finish first React Project

--- src/components/Column/Column.js
+++ src/components/Column/Column.js
@@ -1,4 +1,7 @@
 import styles from './Column.module.scss'
+import Card from '../Card/Card';
+import CardForm from '../CardForm/CardForm';
+import List from '../List/List';
 
 const Column = props => {
     return (
@@ -6,6 +9,10 @@ const Column = props => {
             <h2 className={styles.title}>
                 <span className={styles.icon + ' fa fa-' + props.icon} />
                 {props.title}</h2>
+            <ul className={styles.cards}>
+	            {props.cards.map(card => <Card key={card.id} title={card.title} />)}
+            </ul>
+            <CardForm columnId={props.id} action={props.addCard} />
         </article>
     );
 };
