c88425a8f28af4bb97672bdc703a2cabcf925a96
Dariusz Malinowski <dariusz.m.malinowski@gmail.com>
2026-01-19 16:41:13 +0100

    end chapter 14.6

--- src/components/ColumnForm/ColumnForm.js
+++ src/components/ColumnForm/ColumnForm.js
@@ -1,18 +1,29 @@
 import styles from './ColumnForm.module.scss';
 import { useState } from 'react';
+import Button from '../Button/Button';
 
 const ColumnForm = props => {
-    const [value, setValue] = useState('');
+    const [title, setTitle] = useState('');
+    const [icon, setIcon] = useState('');
+
     const handleSubmit = e => {
         e.preventDefault();
-        props.action({ title: value });
-        setValue('');
+        props.action({ title: title, icon: icon });
+        setTitle('');
+        setIcon('');
     };
 
 	return (
-        <form onSubmit={handleSubmit}>
-            <input type="text" value={value} onChange={e => setValue(e.target.value)} />
-            <button>Add column</button>
+       <form onSubmit={handleSubmit} className={styles.columnForm}>
+            <div className={styles.inputGroup}>
+                <span className={styles.label}>Title:</span>
+                <input className={styles.input} type="text" value={title} onChange={e => setTitle(e.target.value)} />
+            </div>
+            <div className={styles.inputGroup}>
+                <span className={styles.label}>Icon:</span>
+                <input className={styles.input} type="text" value={icon} onChange={e => setIcon(e.target.value)} />
+            </div>
+            <Button>Add column</Button>
         </form>
 	);
 };
