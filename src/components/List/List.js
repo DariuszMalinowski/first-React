8e935cce9cb6c1268e1e3681cb79b91a098f821f
Dariusz Malinowski <dariusz.m.malinowski@gmail.com>
2026-01-28 18:34:08 +0100

    add store.js

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,12 +1,14 @@
 import styles from './List.module.scss';
 import Column from './../Column/Column';
 import ColumnForm from './../ColumnForm/ColumnForm';
-import { useState } from 'react';
-import shortid from 'shortid';
+//import { useState } from 'react';
+//import shortid from 'shortid';
+import { useSelector } from 'react-redux';
 
 const List = () => {
 
-    const [columns, setColumns] = useState([
+	const columns = useSelector(state => state.columns);
+    /*const [columns, setColumns] = useState([
 	{
 		id: 1,
 		title: 'Books',
@@ -36,6 +38,7 @@ const List = () => {
 	}
 	]);
 
+
 	const addColumn = newColumn => {
     setColumns([...columns, { id: shortid(), title: newColumn.title, icon: newColumn.icon, cards: [] }]);
 };
@@ -52,18 +55,21 @@ const List = () => {
 
 };
 
+*/
 	return (
-		<div className={styles.list}>
-			<header className={styles.header}>
-				<h2 className={styles.title}>Things to do<span>soon!</span></h2>
-			</header>
-			<p className={styles.description}>Interesting things I want to check out</p>
-			<section className={styles.columns}>
-				{columns.map(column => <Column key={column.id} id={column.id} title={column.title} icon={column.icon} cards={column.cards} addCard={addCard}/>)}
-			</section>
-			<ColumnForm action={addColumn} />
-		</div>
+  		<div className={styles.list}>
+    		<header className={styles.header}>
+      			<h2 className={styles.title}>Things to do<span>soon!</span></h2>
+    		</header>
+    		<p className={styles.description}>Interesting things I want to check out</p>
+    		<section className={styles.columns}>
+      			{columns.map(column =>
+        			<Column key={column.id} {...column}  />
+      			)}
+    		</section>
+    		<ColumnForm />
+  		</div>
 	);
-};
+}
 
 export default List;
